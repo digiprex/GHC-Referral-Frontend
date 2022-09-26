63b71b02f4dbdb026aca5fa49538d4a1c6d9dc8b
lava <lavakumared15@gmail.com>
2022-09-26 19:06:29 +0530

    user phone number api

--- src/Components/PhoneNumberPopup.jsx
+++ src/Components/PhoneNumberPopup.jsx
@@ -3,22 +3,24 @@ import React from 'react'
 const PhoneNumberPopup = ({phoneNumber,SetPhoneNumber,isValidInput,changePhoneNumber}) => {
   return (
     <>
-			<div className='phone-number-popup-heading'>
-				Change phone number
-			</div>
-			<div className='input-phone-popup'>
-				<input type="text" className='input-phone-text' value={phoneNumber}
-           onChange={SetPhoneNumber} 
-          placeholder="Enter your phone number" inputMode="numeric"
-          pattern="[1-9]{1}[0-9]{5}"
-          minLength="10"
-          maxLength="10" onKeyDown={isValidInput}
-          autoComplete='off'/>
-			</div>
-			<button onClick={(event) => changePhoneNumber(event,true)} disabled={phoneNumber?.length != 10}
-			 className={ ` phone-number-popup ${(phoneNumber?.length == 10)? 'phone-number-submit-correct': 'phone-number-submit' }`}>
-				Submit
-			</button>
+      <div className='phone-number-popup-div'>
+        <div className='phone-number-popup-heading'>
+          Change phone number
+        </div>
+        <div className='input-phone-popup'>
+          <input type="text" className='input-phone-text' value={phoneNumber}
+            onChange={SetPhoneNumber} 
+            placeholder="Enter your phone number" inputMode="numeric"
+            pattern="[1-9]{1}[0-9]{5}"
+            minLength="10"
+            maxLength="10" onKeyDown={isValidInput}
+            autoComplete='off'/>
+        </div>
+        <button onClick={(event) => changePhoneNumber(event,true)} disabled={phoneNumber?.length != 10}
+        className={ ` phone-number-popup ${(phoneNumber?.length == 10)? 'phone-number-submit-correct': 'phone-number-submit' }`}>
+          Submit
+        </button>
+      </div>
     </>
   )
 }
