237b4a29de3b29c0cf20405b6fc7e9026267cecf
Lava <lava.kumar@digi-prex.com>
2022-10-04 12:40:23 +0530

    Refer a friend button change

--- src/Components/ReferAFriend.jsx
+++ src/Components/ReferAFriend.jsx
@@ -90,6 +90,7 @@ export default function ReferAFriend({ customer_id,inHistory,cashName,referral_c
                 </button>
             </div>
           </div> : null }
+          { !customer_id &&
           <div className="refer-friend-container">
             <div className={`referFriend ${customer_id ? "refer-friend-width-with-customer-id" : 
           "refer-friend-width-no-customer-id"}`} onClick={() => share()}> 
@@ -99,6 +100,7 @@ export default function ReferAFriend({ customer_id,inHistory,cashName,referral_c
                 </div>
             </div>
           </div>
+          }
         </div>
       </div>
       <BottomSheet open={open} onDismiss={closeMobileModal}>
